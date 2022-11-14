6dcc4bc6d18c1f304fe3b608b9cfaa6829ffcb8b
dntcuervo <dastringa03@gmail.com>
2022-11-14 16:15:32 -0300

    Deploy 4

--- src/app/service/consulta.service.ts
+++ src/app/service/consulta.service.ts
@@ -13,12 +13,12 @@ export class ConsultaService {
 
   //URL: string = 'https://app-ng-mean.herokuapp.com/api'
 
-  URL = environment.URL
+  //URL = environment.URL
 
   constructor(private http: HttpClient) { }
 
   guardarConsulta(consulta: Consulta): Observable<any> {
-    return this.http.post(this.URL, consulta)
+    return this.http.post("https://app-ng-mean.herokuapp.com/api", consulta)
   }
 
 }
